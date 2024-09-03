e774c4b58f0c613ce937906e419a60c1e249ef5a
Ankit Sharma <ankitconnect10@gmail.com>
2024-09-04 02:22:02 +0530

    Add job card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import Navbar from '../Components/Navbar'
 import './App.css'
+import Card from '../Components/Card1'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      
+      <Card/>
       <Navbar/>
     </>
   )
