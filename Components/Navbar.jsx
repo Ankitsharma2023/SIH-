5c8657fe997dc6b77c8d078b7b2d2ef6ce44a40a
Ankit Sharma <ankitconnect10@gmail.com>
2024-09-03 20:48:45 +0530

    Navbar

---
+++ Components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+
+export default function (){
+
+
+return(
+
+<>
+<nav className="navbar">
+
+<img src = "profile.png" alt =""/>
+
+{/* sunn abhi baad mein change kariyo ! ....ek baar mein git pr dal deta hu  */}
+
+<ul className="nav_list">
+    
+<li className="home">
+    <a href="home.html">Home</a>
+</li>
+
+
+
+<li className="job">
+    <a href="job.html">Job</a>
+</li>
+
+
+
+<li className="course">
+    <a href="course.html">Course</a>
+</li>
+
+
+
+
+<li className="skill_asses">
+    <a href="skill_asses">Skill_Asses</a>
+</li>
+
+
+</ul>
+
+
+
+
+</nav>
+
+
+
+</>
+
+
+)
+
+
+
+
+}
